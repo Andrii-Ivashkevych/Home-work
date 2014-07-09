da0947f370ed886ace6ae06c69ddd37904411573
Andrii-Ivashkevych <Andrii_Ivashkevych@epam.com>
2014-07-09 13:30:16 +0300

    initial project version

---
+++ scripts/load.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+
+    $(document).ready(function () {
+
+        $("#newslinks a").click(function () {
+
+            loadPage($(this));
+            return false;
+
+        });
+
+        function loadPage(link) {
+            var url = link.attr("href");
+            $("#headlines").load(url + " .newsItem");
+        }
+
+    });
+
+})()
